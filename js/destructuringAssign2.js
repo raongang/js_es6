73d5bcad195a9de93f0c22a4362a1ad116556986
raongang <sayllclubs@naver.com>
2021-02-18 01:03:51 +0900

    no message

--- js/destructuringAssign2.js
+++ js/destructuringAssign2.js
@@ -10,10 +10,9 @@
 //   const tiger = animals.tiger;
 //   console.log(cat); // CAT
 //   console.log(dog); // DOG
-//   console.log(tiger); // TIGER
+//   console.log(tiger); // TIGERz
 
-
-
+ 
 //위와 같이 작성하면 비구조화 할당을 수행하며, 배열의 경우 좌항의 index값에 값에 할당되었다면, 객체는 같은 key에 있는 값이 담긴다.
 //   const { cat, dog, tiger } = {
 //     cat: "CAT",
@@ -52,13 +51,29 @@
 // 객체 비구조화시에 변수 선언 키워드가 없는 경우
 //아래처럼 const를 안쓰고 하면 ()를 넣어줘야 한다.
 //let temp1, temp2;
-const { temp1, temp2 } = { temp1: "CAT", temp2: "dog" }
+//const { temp1, temp2 } = { temp1: "CAT", temp2: "dog" }
 
-console.log(temp1);
-console.log(temp2);
+//console.log(temp1);
+//console.log(temp2);
 
-console.log("========================");
+//console.log("========================");
 
-({ temp3, temp4 } = { temp3: "CAT2", temp4: "dog2" })
-console.log(temp3);
-console.log(temp4);
+// ({ temp3, temp4 } = { temp3: "CAT2", temp4: "dog2" })
+// console.log(temp3);
+// console.log(temp4);
+
+//console.log("========================");
+
+
+state = {
+    editing: false,
+    name: "",
+    phone: "",
+};
+//console.log(state);
+//const { editing } = state
+//console.log(editing);
+
+const { c, d, e = 3 } = { c: 1 };
+console.log(d); // undefined
+console.log(e); // 3
